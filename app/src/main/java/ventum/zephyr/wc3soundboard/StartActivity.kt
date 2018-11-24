dba465880dcc11808a808a21e3dbf38f7ef285c0
Alexander Klimenko <ikytosan0@gmail.com>
2018-11-24 16:42:18 +0200

    init project

---
+++ app/src/main/java/ventum/zephyr/wc3soundboard/StartActivity.kt
@@ -0,0 +1,11 @@
+package ventum.zephyr.wc3soundboard
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class StartActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
