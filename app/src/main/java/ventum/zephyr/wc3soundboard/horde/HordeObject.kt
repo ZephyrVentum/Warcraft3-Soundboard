0b09f75d69473c0769a0bcdade5ccaea6c0f16c4
Alexander Klimenko <ikytosan0@gmail.com>
2019-02-24 18:48:08 +0200

    transleted horde sounds

--- app/src/main/java/ventum/zephyr/wc3soundboard/horde/HordeObject.kt
+++ app/src/main/java/ventum/zephyr/wc3soundboard/horde/HordeObject.kt
@@ -25,9 +25,7 @@ object HordeObject {
         add(SoundItem(drawables[i++ % drawables.size], R.raw.gruntnofood1, R.string.gruntnofood1))
         add(SoundItem(drawables[i++ % drawables.size], R.raw.gruntnogold1, R.string.gruntnogold1))
         add(SoundItem(drawables[i++ % drawables.size], R.raw.gruntnolumber1, R.string.gruntnolumber1))
-        add(SoundItem(drawables[i++ % drawables.size], R.raw.gruntresearchcomplete1, R.string.gruntresearchcomplete1))
         add(SoundItem(drawables[i++ % drawables.size], R.raw.grunttownattack1, R.string.grunttownattack1))
         add(SoundItem(drawables[i++ % drawables.size], R.raw.gruntunitattack1, R.string.gruntunitattack1))
-        add(SoundItem(drawables[i++ % drawables.size], R.raw.gruntupgradecomplete1, R.string.gruntupgradecomplete1))
     }
 }
